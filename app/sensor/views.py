4671dd8de38f538961a52b948ad0b5fb0235623d
Vzinik <aliksardar27@gmail.com>
2024-11-17 15:47:47 +0530

    add record data logic

--- app/sensor/views.py
+++ app/sensor/views.py
@@ -1,5 +1,10 @@
+from flask import request, jsonify
+
 from . import sensor
+from app.dal.sensor_client import insert_sensor_data
 
 @sensor.route('/record', methods= ['POST'])
 def record():
-    pass
+    data = request.get_json()
+    if insert_sensor_data(data):
+        return jsonify({"status" : "success"})
