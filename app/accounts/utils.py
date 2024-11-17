d2370ea7682aed0dfff31620258bb8e74a0f7941
Vzinik <aliksardar27@gmail.com>
2024-11-17 15:41:35 +0530

    add password hashing and checking function

--- app/accounts/utils.py
+++ app/accounts/utils.py
@@ -1,2 +1,9 @@
-def check_password(password, password_hash):
-    return True
+from app import bcrypt
+
+
+def hash_password(password):
+    password_hash = bcrypt.generate_password_hash(password)
+    return  password_hash
+
+def check_password(password_hash, password):
+    return bcrypt.check_password_hash(password_hash, password)
