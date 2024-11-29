65daec355ba27518c7b770cb1e2a6f1498806a3c
Vzinik <aliksardar27@gmail.com>
2024-11-29 23:43:17 +0530

    replace render_template() with make_response()

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
-from flask import render_template
+from flask import render_template, make_response
 
 from . import main
 
 @main.route('/')
 def home():
-    return render_template('home.html', title= 'home')
+    http_response = make_response(render_template('home.html', title= 'home'))
