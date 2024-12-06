687f242a21048b341294fd7562eb0aa76536233a
Vzinik <aliksardar27@gmail.com>
2024-12-06 21:52:55 +0530

    add get_sensor_data and insert_sensor_data function

--- app/dal/sensor_client.py
+++ app/dal/sensor_client.py
@@ -1,6 +1,50 @@
-def get_sensor_data():
-    pass
+# A response object in get_sensor_data looks like this
+# response = { 
+#     "data":[
+#         {
+#             "temperature":"",
+#             "pressure":"",
+#             "humidity":"",
+#             "O2":"",
+#             "CO2":"",
+#             "gas":""
+#          },
+#          {
+#             "temperature":"",
+#             "pressure":"",
+#             "humidity":"",
+#             "O2":"",
+#             "CO2":"",
+#          },
+#     ],
+#     "status":{
+#         "message" : "",
+#         "code": 200
+#     }
+# }
+import requests
+from flask import current_app, jsonify
 
-def insert_sensor_data(data):
-    
-    return True
+from utils import validate_device_user, validate_device_token
+
+
+def get_sensor_data(user_id, device_id):
+    if validate_device_user(user_id, device_id):
+        try:    
+            url = current_app.config['URL_SENSOR_DATA']+f"/{device_id}"
+            response = requests.get(url=url)       
+            return response
+        except Exception as e:
+            return {"data":[], "status":{"message": "Something went wrong", "code": 500}}
+    return {"data":[], "status":{"message": "Not found", "code": 404}}
+
+
+def insert_sensor_data(device_id:str, device_token:str, data:dict):
+    if validate_device_token(device_token):
+        try:
+            url = current_app.config['URL_SENSOR_DATA']+f"/{device_id}"
+            response = requests.post(url=url, data =data)
+            return response
+        except Exception as e:
+            return {"message": "Something went wrong", "status": 500}
+    return {"message": "Not found","status": 404}
